b50d17232b533a835c5a85f76abb9887ab5d74a9
antlammi <antti.lammi@helsinki.fi>
2019-02-10 14:44:13 +0200

    numeron päivitys tilapäisesti poistettu

--- build/precache-manifest.40fa02f7592897b145d5a4351d8e94b7.js
+++ build/precache-manifest.40fa02f7592897b145d5a4351d8e94b7.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "ef7c7f19eab37eaeff5f",
-    "url": "/static/js/main.ef7c7f19.chunk.js"
+    "revision": "a7e0acaad1d22cff996e",
+    "url": "/static/js/main.a7e0acaa.chunk.js"
   },
   {
     "revision": "c8a2eede06936c7f61fd",
     "url": "/static/js/1.c8a2eede.chunk.js"
   },
   {
-    "revision": "ef7c7f19eab37eaeff5f",
+    "revision": "a7e0acaad1d22cff996e",
     "url": "/static/css/main.ceaaae55.chunk.css"
   },
   {
-    "revision": "8028f6d4daa7afe98426792c7e641901",
+    "revision": "1b4559c33c188c9dc403f4b22367fe52",
     "url": "/index.html"
   }
 ];
